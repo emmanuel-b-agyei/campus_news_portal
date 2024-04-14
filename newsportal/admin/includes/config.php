c1eaf0e9bf55c780cd068cf5c6f9888f4a7e321b
Emmanuel B. Agyei <91382543+emmanuel-b-agyei@users.noreply.github.com>
2024-04-14 09:46:25 +0000

    Update config.php

--- newsportal/admin/includes/config.php
+++ newsportal/admin/includes/config.php
@@ -1,16 +1,16 @@
 <?php
 // Check if constants are not defined before defining them
 if (!defined('DB_SERVER')) {
-    define('DB_SERVER','localhost');
+    define('DB_SERVER','aucampusne-d590d19f01f6c14a97913087-dbserver.mysql.database.azure.com');
 }
 if (!defined('DB_USER')) {
-    define('DB_USER','root');
+    define('DB_USER','rjhssdtnxn');
 }
 if (!defined('DB_PASS')) {
-    define('DB_PASS','');
+    define('DB_PASS','8PbYf0ZkBBw$oxhK');
 }
 if (!defined('DB_NAME')) {
-    define('DB_NAME','newsportal');
+    define('DB_NAME','aucampusne_d590d19f01f6c14a97913087_database');
 }
 
 // Create database connection
